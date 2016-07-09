8d2d0bee489402edca69404f72b77c8ba6e01c99
EdgarDominguez <edgardominguezmonzon@gmail.com>
2016-07-09 16:03:28 -0600

    presentacion

---
+++ js/eventos.js
@@ -0,0 +1,58 @@
+var empiezaApp = function()
+{
+	$("#login").addClass('active');
+	$("#divlogin").show('fast');
+	$("#liga").removeClass('active');
+	var cargar = function()
+	{
+		$("#logocargando").hide("fast");//ocultamos
+		$("#registro").show("fast");//mostramos
+		$("#logo").show("fast");
+	}
+	setTimeout(cargar, 1000);
+
+	var noLogin =function()
+	{
+		$("#liga").addClass('active');
+		$("#divlogin").hide("fast");
+		$("#divliga").show('fast');
+		$("#login").removeClass('active');
+	}
+	var continuarFAce =function()
+	{
+		$("#liga").addClass('active');
+		$("#divlogin").hide("fast");
+		$("#divliga").show('fast');
+		$("#login").removeClass('active');
+	}
+
+	var Consultas =function()
+	{
+
+	}
+
+	var Entrar =function()
+	{
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$("#btnNoLogin").on("click",noLogin);
+	$("#btncontinuarFace").on("click",continuarFAce);
+	$("#selectLiga").on("click",Consultas);
+	$("#btnEnviar").on("click",Entrar);
+}
+//inicializamos el JQuery
+$(document).on("ready",empiezaApp);
