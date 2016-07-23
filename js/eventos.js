dc1671025f0d15c96a5750bce6c933b16e2f936f
EdgarDominguez <edgardominguezmonzon@gmail.com>
2016-07-23 15:42:36 -0600

    cuerta pantalla

--- js/eventos.js
+++ js/eventos.js
@@ -3,6 +3,7 @@ var empiezaApp = function()
 	$("#login").addClass('active');
 	$("#divlogin").show('fast');
 	$("#liga").removeClass('active');
+
 	var cargar = function()
 	{
 		$("#logocargando").hide("fast");//ocultamos
@@ -33,21 +34,12 @@ var empiezaApp = function()
 
 	var Entrar =function()
 	{
-
+		$("#logo").hide("fast");
+		$("#registro").hide("fast");
+		$("#x").show("slow");
+		$(".menu").show("slow");
+		$(".comentarios").show("slow");
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	$("#btnNoLogin").on("click",noLogin);
 	$("#btncontinuarFace").on("click",continuarFAce);
